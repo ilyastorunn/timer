0a73c76768f08242af331dfd6c9d1a3b6cb32659
İlyas Torun <your-email@example.com>
2025-05-26 16:43:48 +0300

    add shadcn

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,15 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Home() {
   return (
     <>
@@ -8,6 +20,28 @@ export default function Home() {
         <span className="text-2xl text-[#D4D4D4] font-semibold italic">
           track and record your work times on one page
         </span>
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Create project</CardTitle>
+            <CardDescription>
+              Deploy your new project in one-click.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Name</Label>
+                  <Input id="name" placeholder="Name of your project" />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button variant="outline">Cancel</Button>
+            <Button>Deploy</Button>
+          </CardFooter>
+        </Card>
       </div>
     </>
   );
