a3a35abdaf2f78a9decfd8022dc920c1deaee2e7
İlyas Torun <your-email@example.com>
2025-05-26 16:32:37 +0300

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <div className="bg-[#0A0A0A] h-screen flex flex-col items-center relative">
+        <span className="text-5xl text-[#D4D4D4] font-semibold italic">
+          timer
+        </span>
+        <span className="text-2xl text-[#D4D4D4] font-semibold italic">
+          track and record your work times on one page
+        </span>
+      </div>
+    </>
+  );
+}
